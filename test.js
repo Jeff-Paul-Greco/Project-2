6418dbce9a4629a4eb4acdfb172a2d815a52ed13
Marty McFly <mirko.maya@live.com>
2019-10-28 20:46:23 -0400

    Created test login page via test.html and test.js.

---
+++ test.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("#submit").on("click", function (event) {
+        event.preventDefault()
+
+        var username = $("#username").val().trim()
+        var password = $("#password").val().trim()
+
+        console.log(username)
+        console.log(password)
+    })
+})
+
+
