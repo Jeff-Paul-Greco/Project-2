6a3b254a0bf64d5aa5cc2e046f34ea67db63e193
Scott Daniels <sdaniels6363@gmail.com>
2019-11-01 17:30:49 -0400

    adds  barcode scanning functions/post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var db = require("../models");
 var axios = require("axios");
+var Users = require('../models/users');
 
 const upcKey = process.env.UPC_KEY; // api key to upcdatabase.org
 
@@ -8,7 +9,7 @@ function upcLookup(barcode) {
   var url = `https://api.upcdatabase.org/product/${barcode}?apikey=${upcKey}`;
   axios
     .get(url)
-    .then(function(response) {
+    .then(function (response) {
       if (response.status === 200) {
         if (response.data.description) {
           return response.data.description;
@@ -21,34 +22,46 @@ function upcLookup(barcode) {
     });
 }
 
-module.exports = function(app) {
+module.exports = function (app) {
   // post for barcode lookup
-  app.post("/api/search", function(req, res) {
+  app.post("/api/search", function (req, res) {
     let barcode = req.body.barcode;
     var item = upcLookup(barcode);
     res.json(item);
   });
 
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  app.post("/api/newUser", function (req, res) {
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+    // <?> is lines 9-14 the same thing as 16- just using sequelize?
+    //   let dbQuery = "INSERT INTO users (username, password, token) VALUES (?,?,?)"
+    //   connection.query(dbQuery, [req.body.username, req.body.password, req.body.token], function(err,res){
+    //     if(err) throw err;
+    //     console.log("User successfully added")
+    //     res.end();
+    //   })
+    // })
+    let user = req.body;
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
-};
+    //adding users to db using sequelize 
+
+    Users.create({
+
+      username: user.username,
+      password: user.password
+    })
+
+    res.status(204).end()
+
+  })
+
+  //get all users in the db
+
+  app.get("/api/:users?", function (req, res) {
+    if (req.params.users) {
+      Users.findAll({}).then(function (res) {
+        res.json(res);
+      })
+    }
+  })
+
+}
