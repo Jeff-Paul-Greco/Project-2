2b2f2eed2e1f6617adcea97de39dc9dd32d34751
Scott Daniels <sdaniels6363@gmail.com>
2019-11-01 16:35:07 -0400

    added route and function for upc lookup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,34 @@
+require("dotenv").config();
 var db = require("../models");
+var axios = require("axios");
+
+const upcKey = process.env.UPC_KEY; // api key to upcdatabase.org
+
+function upcLookup(barcode) {
+  var url = `https://api.upcdatabase.org/product/${barcode}?apikey=${upcKey}`;
+  axios
+    .get(url)
+    .then(function(response) {
+      if (response.status === 200) {
+        if (response.data.description) {
+          return response.data.description;
+        } else {
+          return "Item description not found.";
+        }
+      } else {
+        return "Invalid Request";
+      }
+    });
+}
 
 module.exports = function(app) {
+  // post for barcode lookup
+  app.post("/api/search", function(req, res) {
+    let barcode = req.body.barcode;
+    var item = upcLookup(barcode);
+    res.json(item);
+  });
+
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
