e0461fa61a0547c60c9abc59be4a000a07dcea57
Scott Daniels <sdaniels6363@gmail.com>
2019-11-02 12:50:06 -0400

    combined fields from user.js to resolve conflicts

--- models/user.js
+++ models/user.js
@@ -1,22 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     username: {
       type: DataTypes.STRING,
-      primaryKey: true
+      primaryKey: true,
+      allowNull: false
     },
-
     password: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false
     },
 
     token: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
     }
   });
 
-  User.associate = function(models) {
+
+  User.associate = function (models) {
     // Associating User with Items
-    // When an User is deleted, also delete any associated Items
+    //  When an User is deleted, also delete any associated Items
     User.hasMany(models.Item, {
       onDelete: "cascade"
     });
