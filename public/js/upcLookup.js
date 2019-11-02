a4ce61e6cde65e027aaf10cc68ae02666490d7e7
Scott Daniels <sdaniels6363@gmail.com>
2019-11-02 13:46:14 -0400

    empty div and readd

--- public/js/upcLookup.js
+++ public/js/upcLookup.js
@@ -1,18 +1,21 @@
-$(document).on("ready", function() {
-  $("#submit").on("click", function(event) {
-    event.preventDefault();
+console.log("loaded upcLookup.js");
 
-    let barcode = $("#item-lookup").value;
 
-    $.ajax({
-      method: "POST",
-      url: "/api/search",
-      data: {
-        barcode: barcode
-      }
-    }).then(function(data) {
-      let html = `<p>${data.data}</p>`;
-      $("#results").append(html);
-    });
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  let barcode = $("#item-search").val().trim();
+  console.log(barcode);
+  $.ajax({
+    method: "POST",
+    url: "/api/search",
+    data: {
+      barcode: barcode
+    }
+  }).then(function(data) {
+    console.log(data);
+    let html = `<p>${data.data}</p>`;
+    $("#results").empty();
+    $("#results").append(html);
   });
 });
