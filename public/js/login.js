dcbfb0a8a65d80ac50b4e731b0249becb5e3b14f
Scott Daniels <sdaniels6363@gmail.com>
2019-11-06 02:12:02 -0500

    added some validation for user creation

--- public/js/login.js
+++ public/js/login.js
@@ -15,32 +15,41 @@ $(document).ready(function () {
       .val()
       .trim()
 
-    var body = {
-      username: username,
-      password: password
-    }
-    console.log(body);
-
-    $.ajax({
-      url: "/login",
-      data: body,
-      method: "POST",
-      success: function (response) {
-        console.log(response);
-        window.localStorage.setItem("username", body.username) // stores the username in local storage.
-        window.localStorage.setItem("userId", response.userId) // sets the local storage for the user
-        window.localStorage.setItem("loggedIn", "true"); // sets the local storage to being logged in.
-        $("#login-invalid").empty(); // if invalid creds pops up, this just empties before redirecting
-        // fill out hidden form and submit it to send the post to /inventory
-        $("#username-inventory").val(body.username);
-        $("#userId-inventory").val(response.userId);
-        $("#hidden-inventory-form").submit();
-      },
-      error: function (response) {
-        $("#login-invalid").text("Invalid Credentials");
+    if ((username === "") && (password === "")) {
+      $("#login-invalid").text("Please enter a valid Username and Password");
+    } else if (username === "") {
+      $("#login-invalid").text("Please enter a username");
+    } else if (password === "") {
+      $("#login-invalid").text("Please enter a password");
+    } else {
+      var body = {
+        username: username,
+        password: password
       }
 
-    });
+      $.ajax({
+        url: "/login",
+        data: body,
+        method: "POST",
+        success: function (response) {
+          console.log(response);
+          window.localStorage.setItem("username", body.username) // stores the username in local storage.
+          window.localStorage.setItem("userId", response.userId) // sets the local storage for the user
+          window.localStorage.setItem("loggedIn", "true"); // sets the local storage to being logged in.
+          $("#login-invalid").empty(); // if invalid creds pops up, this just empties before redirecting
+          // fill out hidden form and submit it to send the post to /inventory
+          $("#username-inventory").val(body.username);
+          $("#userId-inventory").val(response.userId);
+          $("#hidden-inventory-form").submit();
+        },
+        error: function (response) {
+          $("#login-invalid").text("Invalid Credentials");
+        }
+
+      });
+    }
+
+
   });
 
 });
