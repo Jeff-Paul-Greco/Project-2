61868ad90b10550b1951605461444c25082b87c3
Scott Daniels <sdaniels6363@gmail.com>
2019-11-06 16:45:41 -0500

    logout button added

--- public/js/loggedInCheck.js
+++ public/js/loggedInCheck.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 
     if (window.localStorage.getItem("loggedIn") === "true") {
-        // if user is logged in, hide login/register, and show the other two
+        // if user is logged in, hide login/register, and show the other three
         $("#search").show();
         $("#inventory").show();
-    
+        $("#logout").show();
+
         $("#login").hide();
         $("#register").hide();
+        
     } else {
-        // if the user is not logged in, hide inventory and item lookup.
+        // if the user is not logged in, hide inventory, item lookup, and logout.
         $("#search").hide();
         $("#inventory").hide();
-    
+        $("#logout").hide();
     }
 })
