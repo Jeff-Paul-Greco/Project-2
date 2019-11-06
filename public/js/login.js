036430402ce215324b48cff1547a158235f4a5df
Scott Daniels <sdaniels6363@gmail.com>
2019-11-05 22:58:15 -0500

    handles the login

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,9 @@
+window.localStorage.setItem("loggedIn", "false"); // default to not logged in.
+
+
 $(document).ready(function () {
-  $("#login").on("click", function (event) {
+
+  $("#login-btn").on("click", function (event) {
     event.preventDefault();
 
     var username = $("#username")
@@ -9,13 +13,35 @@ $(document).ready(function () {
       .val()
       .trim();
 
-    console.log(username);
-    console.log(password);
+    var body = {
+      username: username,
+      password: password
+    }
+    console.log(body);
+
+    $.ajax({
+      url: "/login",
+      data: body,
+      method: "POST",
+      success: function(response){
+        console.log(response);
+        window.localStorage.setItem("userId", response.userId) // sets the local storage for the user
+        window.localStorage.setItem("loggedIn", "true"); // sets the local storage to being logged in.
+        $("#login-invalid").empty(); // if invalid creds pops up, this just empties before redirecting
+        window.location = "/inventory"; // this sends us to the inventory page after being logged in.
+      },
+      error: function(response){
+        $("#login-invalid").text("Invalid Credentials");
+      }
+
+    });
   });
 
   $("#inventory").hide()
 
   $(".acct-controls-div").hide()
+
+
 
 
 });
